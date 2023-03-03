2cd8b4970e92f613aa2b18fe7b071a69f7c2df8d
Adran Farias Carnavale <adran.carnavale@gmail.com>
2023-03-02 21:14:57 -0300

    updates

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,6 +1,7 @@
 #!/bin/sh
 alias lzg='lazygit'
 alias lzd="lazydocker"
-alias lzn='lazynpm'
+alias extract="tar xzvf"
+alias stow="stow */"
 alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull -q"
 alias db_staging='ssh -L 6543:omega-db-stage.ccujp3gxnlrc.sa-east-1.rds.amazonaws.com:5432 -i /home/$USER/.ssh/bastion-ec2-stage.pem ubuntu@54.232.67.219 -p 32222'
