6f656b5c93632169a2011b6e85ffcf2f9de9fc58
Adran Carnavale <adran.carnavale@gmail.com>
2023-03-03 22:34:20 -0300

    updates

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,11 +1,8 @@
-. "$HOME/.asdf/asdf.sh"
-
 #!/bin/sh
 # HISTFILE="$XDG_DATA_HOME"/zsh/history
 HISTSIZE=1000000
 SAVEHIST=1000000
-export EDITOR="lvim"
-export TERMINAL="alacritty"
+export EDITOR="nvim"
+export TERMINAL="kitty"
 export BROWSER="google-chrome"
 export PATH="$HOME/.local/bin":$PATH
-export PATH="$HOME/.asdf/shims":$PATH
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -9,9 +9,8 @@ plug "$HOME/.config/zsh/aliases.zsh"
 plug "$HOME/.config/zsh/exports.zsh"
 
 # plugins
+plug "zap-zsh/atmachine-prompt"
 plug "zsh-users/zsh-autosuggestions"
-plug "zap-zsh/vim"
-plug "zap-zsh/zap-prompt"
 plug "zap-zsh/fzf"
-plug "zap-zsh/exa"
 plug "zsh-users/zsh-syntax-highlighting"
+plug "zap-zsh/exa"
